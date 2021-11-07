646265791a184fc34c569bf45fe29f93c5afc9f9
soxoj <31013580+soxoj@users.noreply.github.com>
2021-11-07 18:14:48 +0300

    Added export of cli entrypoint

--- maigret/__init__.py
+++ maigret/__init__.py
@@ -8,5 +8,6 @@ __author_email__ = 'soxoj@protonmail.com'
 
 from .__version__ import __version__
 from .checking import maigret as search
+from .maigret import main as cli
 from .sites import MaigretEngine, MaigretSite, MaigretDatabase
 from .notify import QueryNotifyPrint as Notifier
