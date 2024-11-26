57331ecaeecf0bd1556823f6adb864c04be74a07
Soxoj <soxoj@protonmail.com>
2024-11-26 13:33:46 +0100

    Sites checks fixes (#1896)
    Fixed incorrect site names, added method to compare sites

--- utils/update_site_data.py
+++ utils/update_site_data.py
@@ -137,7 +137,7 @@ Rank data fetched from Alexa by domains.
             site_file.write(f'1. {favicon} [{site}]({url_main})*: top {valid_rank}{tags}*{note}\n')
             db.update_site(site)
 
-        site_file.write(f'\nThe list was updated at ({datetime.now(timezone.utc)} UTC)\n')
+        site_file.write(f'\nThe list was updated at ({datetime.now(timezone.utc).date()} UTC)\n')
         db.save_to_file(args.base_file)
 
         statistics_text = db.get_db_stats(is_markdown=True)
