0131f0b64cf2e37d542d3324990313e371c2ef9f
Soxoj <31013580+soxoj@users.noreply.github.com>
2026-04-21 19:03:45 +0200

    Add OnlyFans with activation mechanism; updated site ranks (#2546)

--- maigret/activation.py
+++ maigret/activation.py
@@ -7,7 +7,7 @@ from aiohttp import CookieJar
 
 class ParsingActivator:
     @staticmethod
-    def twitter(site, logger, cookies={}):
+    def twitter(site, logger, cookies={}, **kwargs):
         headers = dict(site.headers)
         del headers["x-guest-token"]
         import requests
@@ -19,7 +19,7 @@ class ParsingActivator:
         site.headers["x-guest-token"] = guest_token
 
     @staticmethod
-    def vimeo(site, logger, cookies={}):
+    def vimeo(site, logger, cookies={}, **kwargs):
         headers = dict(site.headers)
         if "Authorization" in headers:
             del headers["Authorization"]
@@ -31,7 +31,58 @@ class ParsingActivator:
         site.headers["Authorization"] = "jwt " + jwt_token
 
     @staticmethod
-    def weibo(site, logger):
+    def onlyfans(site, logger, url=None, **kwargs):
+        # Signing rules (static_param / checksum_indexes / checksum_constant / format / app_token)
+        # live in data.json under OnlyFans.activation and rotate upstream every ~1–3 weeks.
+        # If "Please refresh the page" keeps firing after activation, refresh them from:
+        #   https://raw.githubusercontent.com/DATAHOARDERS/dynamic-rules/main/onlyfans.json
+        import hashlib
+        import secrets
+        import time as _time
+        from urllib.parse import urlparse
+
+        import requests
+
+        act = site.activation
+        static_param = act["static_param"]
+        indexes = act["checksum_indexes"]
+        constant = act["checksum_constant"]
+        fmt = act["format"]
+        init_url = act["url"]
+
+        user_id = site.headers.get("user-id", "0") or "0"
+
+        def _sign(path):
+            t = str(int(_time.time() * 1000))
+            msg = "\n".join([static_param, t, path, user_id]).encode()
+            sha = hashlib.sha1(msg).hexdigest()
+            cs = sum(ord(sha[i]) for i in indexes) + constant
+            return t, fmt.format(sha, abs(cs))
+
+        if site.headers.get("x-bc", "").strip("0") == "":
+            site.headers["x-bc"] = secrets.token_hex(20)
+
+        if not site.headers.get("cookie"):
+            init_path = urlparse(init_url).path
+            t, sg = _sign(init_path)
+            hdrs = dict(site.headers)
+            hdrs["time"] = t
+            hdrs["sign"] = sg
+            hdrs.pop("cookie", None)
+            r = requests.get(init_url, headers=hdrs, timeout=15)
+            jar = "; ".join(f"{k}={v}" for k, v in r.cookies.items())
+            if jar:
+                site.headers["cookie"] = jar
+                logger.debug(f"OnlyFans init: got cookies {list(r.cookies.keys())}")
+
+        target_path = urlparse(url).path if url else urlparse(init_url).path
+        t, sg = _sign(target_path)
+        site.headers["time"] = t
+        site.headers["sign"] = sg
+        logger.debug(f"OnlyFans signed {target_path} time={t}")
+
+    @staticmethod
+    def weibo(site, logger, **kwargs):
         headers = dict(site.headers)
         import requests
 
